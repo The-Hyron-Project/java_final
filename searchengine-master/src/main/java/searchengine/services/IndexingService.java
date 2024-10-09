2fa7d1d5499f3760e1051eb6c480492e62a85b31
elijah <ilia_vorona@mail.ru>
2024-10-09 22:19:31 +0300

    switching to dev

--- searchengine-master/src/main/java/searchengine/services/IndexingService.java
+++ searchengine-master/src/main/java/searchengine/services/IndexingService.java
@@ -22,7 +22,7 @@ import searchengine.model.Status;
 import searchengine.repositories.PagesRepository;
 import searchengine.repositories.SitesRepository;
 
-//тест6
+//тест7
 @AllArgsConstructor
 @Service
 public class IndexingService extends RecursiveAction {
